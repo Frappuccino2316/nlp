04466f24c3bcb177fe3e3b46d1514c3bb463eabb
Frappuccino001 <fumi00hiro@icloud.com>
2022-01-10 14:36:26 +0900

    文字列を変更してみる

--- src/morphological_analyze.py
+++ src/morphological_analyze.py
@@ -1,6 +1,7 @@
 import MeCab
+import collections
 
 wakati = MeCab.Tagger("-Owakati")
-sentence_wakati = wakati.parse("私は猫が好きです").split()
+sentence_wakati = wakati.parse("私はQAエンジニアをしておりソフトウェアテストなど品質保証の支援を行なっています。").split()
 
 print(sentence_wakati)
