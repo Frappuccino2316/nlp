73378c783d92b78327882c589456dc07e075ec32
Frappuccino001 <fumi00hiro@icloud.com>
2022-01-10 15:24:35 +0900

    ファイル名を変更

--- ma/morphological_analyze.py
+++
@@ -1,9 +0,0 @@
-import MeCab
-import collections
-
-wakati = MeCab.Tagger("-Owakati")
-sentence_wakati = wakati.parse("私はQAエンジニアをしておりソフトウェアテストなど品質保証の支援を行なっています。").split()
-
-c = collections.Counter(sentence_wakati)
-
-print(c.most_common(20))
---
+++ ma/wakati.py
@@ -0,0 +1,21 @@
+import MeCab
+import collections
+
+wakati = MeCab.Tagger("-Owakati")
+
+# テキストを読み込み
+f = open('ma/melos.txt')
+text = f.read()
+f.close()
+
+# 改行を削除
+text_removed_new_line = text.replace('\n', '')
+
+# テキストを形態素解析する
+text_wakati = wakati.parse(text_removed_new_line)
+
+# 頻出順に並べる
+c = collections.Counter(text_wakati)
+
+# 上位20件を表示
+print(c.most_common(20))
