50febf88f1fa65c25c815268aa0995e293a5a2c4
Frappuccino001 <fumi00hiro@icloud.com>
2022-01-10 19:02:42 +0900

    品詞はlistで指定する

--- preprocessing/jan.py
+++ preprocessing/jan.py
@@ -3,7 +3,7 @@ from janome.tokenfilter import POSKeepFilter
 
 text = "彼女と国立新美術館へ行った。"
 
-token_filters = [POSKeepFilter('名刺')]
+token_filters = [POSKeepFilter(['名詞', '動詞', '記号'])]
 a = Analyzer(token_filters=token_filters)
 
 for token in a.analyze(text):
